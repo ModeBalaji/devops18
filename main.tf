45281e205f80972b8cd0af7560141a8bb212fc8d
ModeBalaji <modembalajimpl@gmail.com>
2026-02-02 15:22:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0cae6d6fe6048ca2c"
+    image_id           = "ami-0532be01f26a3de55"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "minimajor"
+    key_name = "monokey"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,8 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-097b4975cf1130493", "subnet-01ee7e5094f09f366"]
+     subnets = ["subnet-0a135f12ca23bf9d8", "
+subnet-00d09f01929e75a28"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
